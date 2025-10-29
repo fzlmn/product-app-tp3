38cf2cb221b0e40db83b2cd740c00511b32960f0
Fatima Zahra <ait29lamine5@gmail.com>
2025-10-29 11:35:24 +0100

    project created

---
+++ src/main/java/mvc/webmvcproducts/WebMvcProductsApplication.java
@@ -0,0 +1,13 @@
+package mvc.webmvcproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMvcProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mvc/webmvcproducts/WebMvcProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.webmvcproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
