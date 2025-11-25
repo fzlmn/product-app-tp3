b40547f4a0f3cf542c33d0d5675213fec1c2b6c2
Fatima Zahra <ait29lamine5@gmail.com>
2025-11-25 22:35:05 +0100

    done + edits on the navbar + now when we add a product we are redirected to the list of products

--- src/main/java/mvc/webmvcproducts/web/ProductController.java
+++ src/main/java/mvc/webmvcproducts/web/ProductController.java
@@ -1,24 +1,67 @@
 package mvc.webmvcproducts.web;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import mvc.webmvcproducts.entities.Product;
 import mvc.webmvcproducts.repositories.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
-
 public class ProductController {
     @Autowired
     private ProductRepo productRepo;
-    @GetMapping("/index")
-    public String index(Model model){
+    @GetMapping("/user/index")
+    @PreAuthorize("hasRole('USER')")
+    public String index(Model model) {
         List<Product> products = productRepo.findAll();
-        model.addAttribute("products", products);
+        model.addAttribute("productsList", products);
         return "products";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/user/index";
+    }
+    @PostMapping("/admin/delete")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String delete(@RequestParam(name = "id") Long id){
+        productRepo.deleteById(id);
+        return "redirect:/user/index";
+    }
+    @GetMapping("/admin/newProduct")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "newProduct";
+    }
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/admin/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) return "newProduct";
+        productRepo.save(product);
+        return "redirect:/user/index";
+    }
+    @GetMapping("/unauthorized")
+    public String unauthorized(){
+        return "unauthorized";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "login";
     }
 
 }
